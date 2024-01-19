c7d16a3a48a6cb1ef97351ce133c3abf1564f3d6
Reinin <singhalpranav2016@gmail.com>
2024-01-19 02:48:33 -0500

    feat: Discord OAuth2 implementation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { URL } from 'node:url';
 import { Client, GatewayIntentBits } from 'discord.js';
 import { loadCommands, loadEvents } from './util/loaders.js';
 import { registerEvents } from './util/registerEvents.js';
-import express from "express";
+import express, { application, urlencoded } from 'express';
+import session from 'express-session';
+import crypto from 'crypto';
 
 // Initialize the Discord client
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
@@ -23,19 +25,66 @@ registerEvents(commands, events, client);
 void client.login(process.env.DISCORD_TOKEN);
 
 let ip;
+const secret = crypto.randomBytes(64).toString('hex');
+const authURL = `https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}&scope=identify&state=`
 
-app.get("/", async (req, res) => {
-    ip = '198.98.51.189';
-    const query = await fetch(`https://proxycheck.io/v2/${ip}&short=1&vpn=3`);
-    const data = await query.json();
-    console.log(data);
-    if (data.proxy === "yes" || data.vpn === "yes" || data.type === "TOR") {
-        res.send("flagged");
+app.use(session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get("/login", (req, res) => {
+    const state = crypto.randomBytes(16).toString('hex');
+    console.log(state);
+    req.session.state = state;
+    res.redirect(authURL + state);
+
+});
+
+app.get("/callback", async (req, res) => {
+    if (req.query.code === undefined) {
+        res.status(401).send('Unauthorized');
+        return;
+    }
+    const callbackState = req.query.state;
+    console.log(callbackState);
+
+    if (callbackState === req.session.state) {
+        console.log(`Oauth2 access code is ${req.query.code}`);
+
+        const tokenResponse = await fetch('https://discord.com/api/oauth2/token', {
+            method: 'POST',
+            body: new URLSearchParams({
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                code: req.query.code,
+                grant_type: 'authorization_code',
+                redirect_uri: process.env.REDIRECT_URI,
+                scope: 'identity',
+            }).toString(),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+
+        const oauthResponse = await tokenResponse.json();
+        console.log(oauthResponse);
+
+        ip = '198.98.51.189';
+        const query = await fetch(`https://proxycheck.io/v2/${ip}&short=1&vpn=3`);
+        const data = await query.json();
+        console.log(data);
+        if (data.proxy === "yes" || data.vpn === "yes" || data.type === "TOR") {
+            res.send("flagged");
+        }
+        else {
+            res.send("passed");
+        }
     }
     else {
-        res.send("passed");
+        res.status(401).send('OAuth callback failed.');
     }
-
 });
 
 app.listen(port, () => {
