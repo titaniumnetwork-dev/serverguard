a3ff35359e523271acd26c9c13d96f0f79532c88
Reinin <singhalpranav2016@gmail.com>
2024-03-23 12:31:32 -0400

    check IP in database

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -53,9 +53,9 @@ export default {
 
     const targetMember = await interaction.guild.members.fetch(interaction.options.getUser("user").id);
 
-    if (targetMember.roles.cache.has(verifiedRole.id)) {
+    if (targetMember.roles.cache.has(verifiedRole.id) || db.checkIp(ip)) {
       return interaction.reply({
-        content: `${targetMember.user.globalName} is already verified.`,
+        content: `${targetMember.user.globalName} or their IP is already verified.`,
         ephemeral: true,
       });
     }
