8faeb52c4d23ed152483d939217b25f6330e5426
David Reed <reedswork@proton.me>
2024-03-26 19:44:57 -0400

    add helper for setting up a database
    this also exposes pool from db.js so it can be reused

--- src/util/db.js
+++ src/util/db.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 import crypto from 'crypto';
 
-const pool = new Pool({
+export const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
