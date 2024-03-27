1b50ebef3286808c5db44f75cc49330af4e7e40e
e9x <reedswork@proton.me>
2024-03-27 21:38:27 +0000

    some changes (#10)
    * parameterized queries
    
    * map the rows
    
    makes the array returned from deletePending() less confusing
    
    * add helper for setting up a database
    
    this also exposes pool from db.js so it can be reused

---
+++ src/db/db.sql
@@ -0,0 +1,10 @@
+-- queries to create a new empty database
+
+CREATE TABLE userdata (
+    id VARCHAR(100),
+    ip VARCHAR(100)
+);
+
+CREATE TABLE pending (
+    id VARCHAR(100)
+);
--- src/util/db.js
+++ src/util/db.js
@@ -1,18 +1,25 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 import crypto from 'crypto';
 
-const pool = new Pool({
+export const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
 
+function hashIp(ip) {
+    const hash = crypto.createHash('sha256');
+    hash.update(process.env.SALT);
+    hash.update(ip);
+    return hash.digest('base64');
+}
+
 export async function checkIp(ip) {
     const client = await pool.connect();
     try {
-        const hashed = crypto.createHash('sha256').update(process.env.SALT + ip).digest('base64');
-        const res = await client.query(`SELECT id FROM userdata WHERE ip = '${hashed}';`);
+        const hashedIp = hashIp(ip);
+        const res = await client.query('SELECT id FROM userdata WHERE ip = $1;', [hashedIp]);
         if (res.rowCount !== 0) {
             return res.rows[0].id;
         }
@@ -27,8 +34,8 @@ export async function checkIp(ip) {
 export async function setData(id, ip) {
     const client = await pool.connect();
     try {
-        const hashedIp = crypto.createHash('sha256').update(process.env.SALT + ip).digest('base64');
-        await client.query(`INSERT INTO userdata VALUES('${id}', '${hashedIp}');`);
+        const hashedIp = hashIp(ip);
+        await client.query('INSERT INTO userdata VALUES($1,$2);', [id,hashedIp]);
     } catch (err) {
         console.error(err);
     } finally {
@@ -39,7 +46,7 @@ export async function setData(id, ip) {
 export async function deleteData(id) {
     const client = await pool.connect();
     try {
-        await client.query(`DELETE FROM userdata WHERE id='${id}';`);
+        await client.query('DELETE FROM userdata WHERE id=$1;', [id]);
     } catch (err) {
         console.error(err);
     } finally {
@@ -50,7 +57,7 @@ export async function deleteData(id) {
 export async function pendingDeletion(id) {
     const client = await pool.connect();
     try {
-        await client.query(`INSERT INTO pending VALUES('${id}');`);
+        await client.query('INSERT INTO pending VALUES($1);', [id]);
     } catch (err) {
         console.error(err);
     } finally {
@@ -63,20 +70,14 @@ export async function deletePending() {
     try {
         const result = await client.query({
             rowMode: 'array',
-            // text: 'DELETE FROM userdata WHERE id IN (select id from pending);'
             text: 'SELECT id FROM pending;'
         });
-        let quereyString = '';
-        if (result.rowCount === 0) {
-            return [[0]];
+        const ids = result.rows.map(row => row[0]);
+        if (ids.length) {
+            await client.query('DELETE FROM userdata WHERE id = ANY ($1);', [ids]);
         }
-        result.rows.forEach(row => {
-            quereyString += `'${row[0]}',`;
-        });
-        quereyString = quereyString.slice(0, -1);
-        await client.query(`DELETE FROM userdata WHERE id IN (${quereyString});`);
-        await client.query(`TRUNCATE table pending;`);
-        return result.rows;
+        await client.query('TRUNCATE table pending;');
+        return ids;
     } catch (err) {
         console.log(err);
     }
@@ -88,7 +89,7 @@ export async function deletePending() {
 export async function cancelPending(id) {
     const client = await pool.connect();
     try {
-        await client.query(`DELETE FROM pending WHERE id='${id}'`);
+        await client.query('DELETE FROM pending WHERE id=$1', [id]);
     }
     catch (err) {
         console.log(err);
