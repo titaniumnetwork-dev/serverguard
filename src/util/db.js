bc391042fc38a2eb4e0d54eaee5a14c9920c5c78
David Reed <reedswork@proton.me>
2024-03-26 23:57:30 -0400

    use single quotes

--- src/util/db.js
+++ src/util/db.js
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 import crypto from 'crypto';
 
 export const pool = new Pool({
@@ -19,7 +19,7 @@ export async function checkIp(ip) {
     const client = await pool.connect();
     try {
         const hashedIp = hashIp(ip);
-        const res = await client.query("SELECT id FROM userdata WHERE ip = $1;", [hashedIp]);
+        const res = await client.query('SELECT id FROM userdata WHERE ip = $1;', [hashedIp]);
         if (res.rowCount !== 0) {
             return res.rows[0].id;
         }
@@ -35,7 +35,7 @@ export async function setData(id, ip) {
     const client = await pool.connect();
     try {
         const hashedIp = hashIp(ip);
-        await client.query("INSERT INTO userdata VALUES($1,$2);", [id,hashedIp]);
+        await client.query('INSERT INTO userdata VALUES($1,$2);', [id,hashedIp]);
     } catch (err) {
         console.error(err);
     } finally {
@@ -46,7 +46,7 @@ export async function setData(id, ip) {
 export async function deleteData(id) {
     const client = await pool.connect();
     try {
-        await client.query("DELETE FROM userdata WHERE id=$1;", [id]);
+        await client.query('DELETE FROM userdata WHERE id=$1;', [id]);
     } catch (err) {
         console.error(err);
     } finally {
@@ -57,7 +57,7 @@ export async function deleteData(id) {
 export async function pendingDeletion(id) {
     const client = await pool.connect();
     try {
-        await client.query("INSERT INTO pending VALUES($1);", [id]);
+        await client.query('INSERT INTO pending VALUES($1);', [id]);
     } catch (err) {
         console.error(err);
     } finally {
@@ -74,9 +74,9 @@ export async function deletePending() {
         });
         const ids = result.rows.map(row => row[0]);
         if (ids.length) {
-            await client.query("DELETE FROM userdata WHERE id = ANY ($1);", [ids]);
+            await client.query('DELETE FROM userdata WHERE id = ANY ($1);', [ids]);
         }
-        await client.query("TRUNCATE table pending;");
+        await client.query('TRUNCATE table pending;');
         return ids;
     } catch (err) {
         console.log(err);
@@ -89,7 +89,7 @@ export async function deletePending() {
 export async function cancelPending(id) {
     const client = await pool.connect();
     try {
-        await client.query("DELETE FROM pending WHERE id=$1", [id]);
+        await client.query('DELETE FROM pending WHERE id=$1', [id]);
     }
     catch (err) {
         console.log(err);
