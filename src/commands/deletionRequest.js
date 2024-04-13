00f186794b710f212dbb529ee9c9f2320c082797
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-04-12 21:02:22 -0400

    feat: add confirmation thing

--- src/commands/deletionRequest.js
+++ src/commands/deletionRequest.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from 'discord.js';
+import { EmbedBuilder, MessageActionRow, MessageButton } from 'discord.js';
 import { pendingDeletion } from '../util/db.js';
 
 /** @type {import('./index.js').Command} */
@@ -8,11 +8,43 @@ export default {
         description: 'Requests the deletion of the user\'s data',
     },
     async execute(interaction) {
-        await pendingDeletion(interaction.user.id);
-        const embed = new EmbedBuilder()
-            .setTitle('Success!')
-            .setDescription('You have successfully submitted a data deletion request. Note that we process requests on the first of each month.')
+        const confirmationEmbed = new EmbedBuilder()
+            .setTitle('Confirmation')
+            .setDescription('Are you sure you want to request the deletion of your data?')
             .setColor('#600080');
-        await interaction.reply({ embeds: [embed], ephemeral: true });
+
+        const confirmButton = new MessageButton()
+            .setCustomId('confirm')
+            .setLabel('Yes')
+            .setStyle('SUCCESS');
+
+        const denyButton = new MessageButton()
+            .setCustomId('deny')
+            .setLabel('No')
+            .setStyle('DANGER');
+
+        const row = new MessageActionRow().addComponents(confirmButton, denyButton);
+        const confirmationMessage = await interaction.reply({ embeds: [confirmationEmbed], components: [row], ephemeral: true });
+        const filter = i => i.customId === 'confirm' || i.customId === 'deny';
+        const collector = interaction.channel.createMessageComponentCollector({ filter });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'confirm') {
+                await pendingDeletion(interaction.user.id);
+                const successEmbed = new EmbedBuilder()
+                    .setTitle('Success!')
+                    .setDescription('You have successfully submitted a data deletion request. Note that we process requests on the first of each month.')
+                    .setColor('#600080');
+                await interaction.editReply({ embeds: [successEmbed], components: [] });
+            } else if (i.customId === 'deny') {
+                await interaction.editReply({ content: 'Deletion request cancelled.', components: [] });
+            }
+        });
+
+        collector.on('end', () => {
+            if (!confirmationMessage.deleted) {
+                confirmationMessage.edit({ components: [] });
+            }
+        });
     },
 };
