c160dd0ad9d5ead2dbf6cc595cb45e332f409b05
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-04-16 19:40:37 -0400

    hotfix: remove extra ;

--- src/commands/deletionRequest.js
+++ src/commands/deletionRequest.js
@@ -12,22 +12,18 @@ export default {
             .setTitle('Confirmation')
             .setDescription('Are you sure you want to request the deletion of your data?')
             .setColor('#600080');
-
         const confirmButton = new MessageButton()
             .setCustomId('confirm')
             .setLabel('Yes')
             .setStyle('SUCCESS');
-
         const denyButton = new MessageButton()
             .setCustomId('deny')
             .setLabel('No')
             .setStyle('DANGER');
-
         const row = new MessageActionRow().addComponents(confirmButton, denyButton);
         const confirmationMessage = await interaction.reply({ embeds: [confirmationEmbed], components: [row], ephemeral: true });
         const filter = i => i.customId === 'confirm' || i.customId === 'deny';
         const collector = interaction.channel.createMessageComponentCollector({ filter });
-
         collector.on('collect', async i => {
             if (i.customId === 'confirm') {
                 await pendingDeletion(interaction.user.id);
@@ -40,7 +36,6 @@ export default {
                 await interaction.editReply({ content: 'Deletion request cancelled.', components: [] });
             }
         });
-
         collector.on('end', () => {
             if (!confirmationMessage.deleted) {
                 confirmationMessage.edit({ components: [] });
