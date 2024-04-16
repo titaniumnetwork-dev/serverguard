a73685a10b5f7301c528ade7ca369fe7cb6e2fd0
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-04-16 19:37:25 -0400

    feat: manual deletion request command

---
+++ src/commands/manualDeletion.js
@@ -0,0 +1,31 @@
+import { EmbedBuilder } from 'discord.js';
+import { deleteData } from '../util/db.js';
+
+/** @type {import('./index.js').Command} */
+export default {
+    data: {
+        name: 'manualdeletion',
+        description: 'Manually deletes a user\'s data from the database.',
+        dm_permission: false,
+    },
+    async execute(interaction) {
+        const user = interaction.options.getMember('user');
+        const userId = user.id;
+        await deleteData(userId)
+            .then(() => {
+                const member = await interaction.guild.members.fetch(userId);
+                if (member.roles.cache.some(role => role.id === process.env.ROLE_ID)) {
+                    member.roles.remove(process.env.ROLE_ID);
+                    console.log('Removed role from ' + userId);
+                }
+                const embed = new EmbedBuilder()
+                    .setTitle('Manual Data Deletion Request Processed.')
+                    .setDescription(`<@${userId}> has been deleted from the database successfully.`)
+                    .setColor('#600080');    
+                interaction.reply({ embeds: [embed], ephemeral: false});
+            })
+            .catch((err) => {
+                interaction.reply({ content: 'An error occurred while deleting user data.', ephemeral: true });
+            });
+    },
+};
