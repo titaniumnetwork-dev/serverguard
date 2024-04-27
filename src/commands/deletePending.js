566134a1817ad18efa92c5de06fb4d2642b84e4a
vys <hey@vyslix.dev>
2024-04-27 13:55:51 -0400

    make the reply defer instead of being an ephemeral

--- src/commands/deletePending.js
+++ src/commands/deletePending.js
@@ -9,26 +9,31 @@ export default {
     dm_permission: false,
   },
   async execute(interaction) {
-    const result = await deletePending();
-    for (const id of result) {
-      if (await interaction.guild.members.fetch(id).catch((err) => false)) {
-        const member = await interaction.guild.members.fetch(id);
-        if (
-          member.roles.cache.some((role) => role.id === process.env.ROLE_ID)
-        ) {
-          member.roles.remove(process.env.ROLE_ID);
-          console.log('Removed role from ' + id);
-        }
-      } else {
-        console.log(id + ' is not in the discord');
+  await interaction.deferReply();
+
+  const result = await deletePending();
+
+  for (const id of result) {
+    if (await interaction.guild.members.fetch(id).catch((err) => false)) {
+      const member = await interaction.guild.members.fetch(id);
+      if (
+        member.roles.cache.some((role) => role.id === process.env.ROLE_ID)
+      ) {
+        member.roles.remove(process.env.ROLE_ID);
+        console.log('Removed role from ' + id);
       }
+    } else {
+      console.log(id + ' is not in the discord');
     }
-    const embed = new EmbedBuilder()
-      .setTitle('Data deletion requests have been processed.')
-      .setDescription(
-        `All users that have requested deletion of their data have been cleared from the database and their roles removed. We have processed ${result.length} requests.`,
-      )
-      .setColor('#600080');
-    await interaction.reply({ embeds: [embed], ephemeral: false });
-  },
+  }
+
+  const embed = new EmbedBuilder()
+    .setTitle('Data deletion requests have been processed.')
+    .setDescription(
+      `All users that have requested deletion of their data have been cleared from the database and their roles removed. We have processed ${result.length} requests.`
+    )
+    .setColor('#600080');
+
+  await interaction.editReply({ embeds: [embed] });
+},
 };
