aedb37d7adbb09edc04ff19ffa6ca2e31e1ef11a
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-06-10 11:16:40 -0400

    why am I retarded lmao

--- src/commands/manualDeletion.js
+++ src/commands/manualDeletion.js
@@ -3,24 +3,27 @@ import { deleteData } from '../util/db.js';
 
 /** @type {import('./index.js').Command} */
 export default {
-    data: {
-        name: 'manualdeletion',
-        description: 'Manually deletes a user\'s data from the database.',
-        dm_permission: false,
-    },
+    data: new SlashCommandBuilder()
+    .setName('manualdeletion')
+    .setDescription('Manually deletes a user\'s data from the database.')
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('The user to earase from the DB')
+        .setRequired(true),
+    ),
     async execute(interaction) {
-        const user = interaction.options.getMember('user');
-        const userId = user.id;
-        await deleteData(userId)
+        const user = interaction.options.getMember('user').id;
+        await deleteData(user)
             .then(async () => {
-                const member = await interaction.guild.members.fetch(userId);
+                const member = await interaction.guild.members.fetch(user);
                 if (member.roles.cache.some(role => role.id === process.env.ROLE_ID)) {
                     member.roles.remove(process.env.ROLE_ID);
-                    console.log('Removed role from ' + userId);
+                    console.log('Removed role from ' + user);
                 }
                 const embed = new EmbedBuilder()
                     .setTitle('Manual Data Deletion Request Processed.')
-                    .setDescription(`<@${userId}> has been deleted from the database successfully.`)
+                    .setDescription(`<@${user}> has been deleted from the database successfully.`)
                     .setColor('#600080');    
                 interaction.reply({ embeds: [embed], ephemeral: false});
             })
