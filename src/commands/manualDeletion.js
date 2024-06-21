482031bd7f5fdd75690637d19ce5ce335154ade3
Reinin <singhalpranav2016@gmail.com>
2024-06-21 11:19:07 -0400

    fix imports

--- src/commands/manualDeletion.js
+++ src/commands/manualDeletion.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from 'discord.js';
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { deleteData } from '../util/db.js';
 
 /** @type {import('./index.js').Command} */
