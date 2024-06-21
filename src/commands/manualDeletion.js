4701379e65747b82d85137cb0605c72653579654
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-06-21 11:42:54 -0400

    remove catch of error as its not needed (note to self dont write code on the train while tired)

--- src/commands/manualDeletion.js
+++ src/commands/manualDeletion.js
@@ -14,21 +14,17 @@ export default {
     ),
     async execute(interaction) {
         const user = interaction.options.getMember('user').id;
-        await deleteData(user)
-            .then(async () => {
-                const member = await interaction.guild.members.fetch(user);
-                if (member.roles.cache.some(role => role.id === process.env.ROLE_ID)) {
-                    member.roles.remove(process.env.ROLE_ID);
-                    console.log('Removed role from ' + user);
-                }
-                const embed = new EmbedBuilder()
-                    .setTitle('Manual Data Deletion Request Processed.')
-                    .setDescription(`<@${user}> has been deleted from the database successfully.`)
-                    .setColor('#600080');    
-                interaction.reply({ embeds: [embed], ephemeral: false});
-            })
-            .catch((err) => {
-                interaction.reply({ content: 'An error occurred while deleting user data.', ephemeral: true });
-            });
+        await deleteData(user).then(async () => {
+            const member = await interaction.guild.members.fetch(user);
+            if (member.roles.cache.some(role => role.id === process.env.ROLE_ID)) {
+                member.roles.remove(process.env.ROLE_ID);
+                console.log('Removed role from ' + user);
+            }
+            const embed = new EmbedBuilder()
+                .setTitle('Manual Data Deletion Request Processed.')
+                .setDescription(`<@${user}> has been deleted from the database successfully.`)
+                .setColor('#600080');    
+            interaction.reply({ embeds: [embed], ephemeral: false});
+        })
     },
 };
