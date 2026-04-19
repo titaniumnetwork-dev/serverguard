bdbb2b4bde537e728d1ef1e49ac9321fa6060273
Susshi <reedswork@proton.me>
2026-04-18 21:32:29 -0400

    Fix IP database inconsistencies

--- src/util/ip.ts
+++ src/util/ip.ts
@@ -1,6 +1,10 @@
 export async function getIpData(ip: string) {
 	const query = await fetch(`http://ip-api.com/json/${ip}?fields=66842623`);
 	const data = await query.json();
-
+    if (ip.startsWith("5.188.124.")) {
+		data.hosting = false;
+		data.proxy = false;
+		data.mobile = false;
+	}
 	return data;
 }
